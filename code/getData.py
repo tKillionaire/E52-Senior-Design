7f3235ce92d4c41718c33474e2c9103c1f1cba5d
ajbetancourt <alexander.betancourt@marquette.edu>
2020-10-03 14:49:10 -0500

    initial commit

---
+++ code/getData.py
@@ -0,0 +1,10 @@
+"""
+make it so it asks to upload a file or start data collection with sensors and CNC
+start with button push
+"""
+
+
+def function getData():
+    #run microcontroller, and collect data into matrix [x,y,z, B-field]
+    #return matrix
+    return
---
+++ code/main_gui.py
@@ -0,0 +1,8 @@
+
+
+
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QGridLayout, QWidget
+
+#4 buttons upload file, new graph, save, save as 
+#visuals cnc conneted/unconnected--needs to be be for new graph 
+
