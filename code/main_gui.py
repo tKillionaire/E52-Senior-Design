105fd805f619dfb2d9c64152ddd827fb63def631
iamnotdavidhasselhoff <43529015+ajbetancourt@users.noreply.github.com>
2020-10-04 19:19:43 -0500

    setting up skeleton

--- code/main_gui.py
+++ code/main_gui.py
@@ -1,8 +1,25 @@
-
-
-
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QGridLayout, QWidget
+import sys
+from plot3D import plot as plt3D
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import numpy as np
 
 #4 buttons upload file, new graph, save, save as 
 #visuals cnc conneted/unconnected--needs to be be for new graph 
 
+def plotData():
+    plt3D()
+
+
+class gui(QtWidgets, QMainWindow):
+    plotData()
+
+
+app = QApplication(sys.argv)
+
+window = gui()
+window.show() # IMPORTANT!!!!! Windows are hidden by default.
+
+# Start the event loop.
+app.exec_()
