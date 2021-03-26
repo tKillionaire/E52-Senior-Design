6188c3d7f9215ccd8beae2374f5ec6a4a60484b0
ajbetancourt <alexander.betancourt@marquette.edu>
2021-03-26 13:26:33 -0500

    add spec analyze file

--- code/main_gui.py
+++ code/main_gui.py
@@ -20,17 +20,19 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 class plot(FigureCanvasQTAgg):
-     def __init__(self, parent=None, width=5, height=4, dpi=100):   
+    def __init__(self, parent=None, width=5, height=4, dpi=100):   
         def randrange(n, vmin, vmax):
             '''
             Helper function to make an array of random numbers having shape (n, )
             with each number distributed Uniform(vmin, vmax).
             '''
             return (vmax - vmin)*np.random.rand(n) + vmin
+        # Get data 
+
 
         fig = plt.figure()
         ax = fig.add_subplot(111, projection='3d')
-
+  
         n = 100
 
         # For each set of style and range settings, plot n random points in the box
@@ -44,18 +46,32 @@ class plot(FigureCanvasQTAgg):
         ax.set_xlabel('X inches ')
         ax.set_ylabel('Y inches')
         ax.set_zlabel('Z inches')
+        #plt.show()
         super(plot, self).__init__(fig)
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
-       
+
         layout = QGridLayout()      
 
         sc = plot(self, width=5, height=4, dpi=100)
-    
+        # def color_gradient():
+        #     #max min value for b field then linear gradient from blue (high) to red(low)
+
+        # def upload():
+        #     #df
+        # def run():
+        #     #df
+
+        # def save():
+        #     #df
+
+
+        toolbar = NavigationToolbar(sc, self)
+
         btnLayout = QHBoxLayout()
-        
+  
         runButton = QPushButton('Run', self)
         #runButton.clicked.connect(runClick) ##fix later 
         btnLayout.addWidget(runButton)
@@ -64,23 +80,31 @@ class MainWindow(QtWidgets.QMainWindow):
         #saveButton.clicked.connect(saveClick) ##fix later 
         btnLayout.addWidget(saveButton)
 
-        ##ADD QComboBox -dropdown menu for sizing/data collection
         uploadButton = QPushButton('Upload', self)
         # uploadButton.clicked.connect(uploadClick) ##fix later
         btnLayout.addWidget(uploadButton)
-       
+     
         combo = QComboBox()
         combo.addItems(['1/8"','1/2"','3/4"','1"'])
         btnLayout.addWidget(combo)
-        
+     
         button_master = QWidget()
         button_master.setLayout(btnLayout)
-        
+    
         layout.addWidget(button_master, 0, 4)
+        layout.addWidget(toolbar, 1, 4)
         layout.addWidget(sc, 2 ,4 ,20 ,4)
         layWidget = QWidget()
         layWidget.setLayout(layout)
         self.setCentralWidget(layWidget)
+    def plot_pts():
+        ''' These are just for plot testing they will be from a txt file, the end goal being to map in 
+        real time --function will call the text file as its being edited, or will upload full txt file'''
+        zvals = [0,0,0,0,1,1,1,1,2,2,2,2,3,3,3,3] 
+        xvals = [0,1,2,3,0,1,2,3,0,1,2,3,0,1,2,3]
+        yvals = [0,1,2,3,4,5,6]
+        b_field = [11,10,9,8,7,6,5,4,3,2,1,0]
+        #for x,y,z,b in zip(xvals,yvals,zvals,b_field):
 
 if __name__ =='__main__':
     app = QtWidgets.QApplication(sys.argv)
